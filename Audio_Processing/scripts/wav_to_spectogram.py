f5647574fe6ad2db7ad0e5ef1770b4177fa4a671
Luca Benjamin <2563233b@student.gla.ac.uk>
2023-10-23 13:00:11 +0100

    Finished STFT pipeline

--- Audio_Processing/scripts/wav_to_spectogram.py
+++ Audio_Processing/scripts/wav_to_spectogram.py
@@ -2,82 +2,83 @@ import os
 import pathlib
 
 import matplotlib.pyplot as plt
-
 import numpy as np
-
 import tensorflow as tf
-
 from scipy.io import wavfile
-def get_spectrogram(waveform):
-  # Convert the waveform to a spectrogram via a STFT.
-  spectrogram = tf.signal.stft(
-      waveform, frame_length=255, frame_step=128)
-  # Obtain the magnitude of the STFT.
-  spectrogram = tf.abs(spectrogram)
-  # Add a `channels` dimension, so that the spectrogram can be used
-  # as image-like input data with convolution layers (which expect
-  # shape (`batch_size`, `height`, `width`, `channels`).
-  spectrogram = spectrogram[..., tf.newaxis]
-  return spectrogram
 
 
-def plot_spectrogram(spectrogram, ax):
-  if len(spectrogram.shape) > 2:
-    assert len(spectrogram.shape) == 3
-    spectrogram = np.squeeze(spectrogram, axis=-1)
-  # Convert the frequencies to log scale and transpose, so that the time is
-  # represented on the x-axis (columns).
-  # Add an epsilon to avoid taking a log of zero.
-  log_spec = np.log(spectrogram.T + np.finfo(float).eps)
-  height = log_spec.shape[0]
-  width = log_spec.shape[1]
-  X = np.linspace(0, np.size(spectrogram), num=width, dtype=int)
-  Y = range(height)
-  ax.pcolormesh(X, Y, log_spec)
+def calculate_frame_step(waveform_length, desired_width=255, frame_length=508):
+    return int(round((waveform_length - frame_length) / (desired_width - 1)))
 
-def spectrogram_to_waveform(spectrogram):
-    # Remove the channel dimension.
-    spectrogram = tf.squeeze(spectrogram, axis=-1)
+
+
+def get_spectrogram(waveform, frame_step):
+    # Convert the waveform to a spectrogram via a STFT.
+    stft_result = tf.signal.stft(
+        waveform, frame_length=508, frame_step=frame_step)
     
-    # Inverse STFT. This will give complex values.
+    magnitude = tf.abs(stft_result)
+    phase = tf.math.angle(stft_result)
+    
+    return magnitude, phase, stft_result
+
+def spectrogram_to_waveform(stft_result, frame_step):
     waveform = tf.signal.inverse_stft(
-        spectrogram, frame_length=255, frame_step=128, window_fn=tf.signal.inverse_stft_window_fn(128)
+        stft_result, frame_length=512, frame_step=frame_step,
+        window_fn=tf.signal.inverse_stft_window_fn(frame_step)
     )
-    
-    # Since we used the magnitude (tf.abs) to get the spectrogram,
-    # the phase information was lost. Therefore, the reconstructed
-    # waveform from the inverse STFT might not be exactly the same
-    # as the original, but it should be a close approximation.
-
     return waveform
 
+def plot_spectrogram(spectrogram, ax):
+    if spectrogram.shape[-1] == 1:
+        spectrogram = np.squeeze(spectrogram, axis=-1)
 
-def save_waveform_to_wav(waveform, filename, sample_rate=44100):
-    # Ensure waveform is a NumPy array and scale to int16.
-    waveform = (waveform * 32767 / max(0.01, np.max(np.abs(waveform)))).astype(np.int16)
+    log_spec = np.log(spectrogram.T + np.finfo(float).eps)
 
-    # Save to WAV file.
+    print("Spectrogram shape:", spectrogram.shape)
+    print("Log_spec shape:", log_spec.shape)
+
+    height = log_spec.shape[0]
+    width = log_spec.shape[1]
+    X = np.linspace(0, np.size(spectrogram), num=width, dtype=int)
+    Y = range(height)
+    ax.pcolormesh(X, Y, log_spec)
+
+
+def save_waveform_to_wav(waveform, filename, sample_rate=16000):
     wavfile.write(filename, sample_rate, waveform)
 
-audio_path = str(pathlib.Path().absolute()) + "/Basic_Audio_Processing/sample_wav_tunes/"
+def stereo_to_mono(waveform):
+    return np.mean(waveform, axis=1)
 
-audio_files = os.listdir(audio_path)
+def main():
+    # Example of reading a WAV file, converting it to a spectrogram, 
+    # and then converting it back to a waveform and saving it
+    sample_rate, waveform = wavfile.read("Audio_Processing\processed_youtube_clips_sample\Piano3.wav_segment_5.wav")
+    waveform = waveform.astype(np.float32)
+    waveform /= 32768.0  # As int16 ranges from -32768 to 32767
 
-sample_rate, data = wavfile.read(audio_path + str(audio_files[0]))
-waveform = data[:, 0]
+    print("Waveform shape:", waveform.shape)  # Add this line
 
+    frame_step = calculate_frame_step(len(waveform))
 
-spectrogram = get_spectrogram(waveform)
+    magnitude, phase, stft_result = get_spectrogram(waveform, frame_step)
 
-fig, axes = plt.subplots(2, figsize=(12, 8))
-timescale = np.arange(waveform.shape[0])
-axes[0].plot(timescale, waveform)
-axes[0].set_title('Waveform')
-axes[0].set_xlim([0, 1000000])
+    print("STFT result shape:", stft_result.shape)  # Add this line
+    
+    # Plot the magnitude spectrogram for visualization
+    fig, ax = plt.subplots(figsize=(10, 4))
+    plot_spectrogram(magnitude.numpy(), ax)
+    ax.set_title("Magnitude Spectrogram")
+    plt.show()
 
-plot_spectrogram(spectrogram, axes[1])
-axes[1].set_title('Spectrogram')
-plt.show()
+    # Convert the spectrogram back to waveform
+    reconstructed_waveform = spectrogram_to_waveform(stft_result, frame_step)
 
-waveform_from_spec = np.array(spectrogram_to_waveform(spectrogram).numpy())
-save_waveform_to_wav(waveform_from_spec, "fullyTransformed.wav")
+    print(reconstructed_waveform.shape[0])
+    
+    # Save the reconstructed waveform as a WAV file
+    save_waveform_to_wav(reconstructed_waveform.numpy(), "reconstructed_file.wav", sample_rate)
+
+if __name__ == "__main__":
+    main()
